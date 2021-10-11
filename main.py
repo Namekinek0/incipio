c18108f7bf444bbc1c77b52e8a2fcddae06de42b
Namekinek0 <antonio.polara@hotmail.it>
2021-10-11 01:18:52 +0100

    added basic commands

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 print('''
  .----------------.  .-----------------. .----------------.  .----------------.  .----------------.  .----------------.  .----------------. 
 | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
@@ -12,6 +14,29 @@ print('''
  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' 
  Incipio Dictionary by Namekinek0 - ver. 0.1
 ''')
-word = input("What lemma do you want to search? ")
-from check import read_dict
-read_dict(word)
+def start():
+    exit = {"--exit", "-e"}
+    help = {"--help", "-h"}
+    version = {"--version", "-v"}
+    word = input("Enter a command or search for a word (type --help for a list of commands): ")
+    word = word.lower()
+    if word in exit:
+        os.abort()
+    if word in help:
+        from help import print_help
+        print_help()
+        print("\n")
+        start()
+    if word in version:
+        print("Incipio Dictionary ver. 0.1")
+        print("\n")
+        start()
+    if word[0] == "-":
+        print("Command not found, try again.")
+        print("\n")
+        start()
+    from check import read_dict
+    read_dict(word)
+    print("\n")
+    start()
+start()
