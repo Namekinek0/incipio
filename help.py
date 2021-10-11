d41b587dfeca84a67f6783a1e25d7ffa6b8f25c9
Namekinek0 <antonio.polara@hotmail.it>
2021-10-11 01:20:15 +0100

    added the help command

---
+++ help.py
@@ -0,0 +1,9 @@
+exit = "--exit, -x"
+help = "--help, -h"
+ver = "--version, -v"
+def print_help():
+    print(
+        f"{'Close this application:':<30}{exit:>50}",
+        f"\n{'Show this list:':<30}{help:>50}",
+        f"\n{'Show the application version:':<30}{ver:>50}"
+    )
